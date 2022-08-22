b3f1bc8c8597d157fc6b48c3006a05baca55c653
Eyyup Ucmaz 0291000445 <eyyup.ucmaz@netaszm.com.tr>
2022-08-22 12:05:04 +0300

    added loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,18 @@
+const Loading = () => {
+  return (
+    <>
+      <div
+        className="inline-block w-20 h-20
+      border-8
+    border-t-amber-500
+    border-r-blue-500
+    border-b-red-500
+    border-l-green-500
+      rounded-full
+      animate-spin"
+      ></div>
+    </>
+  );
+};
+
+export default Loading;
