fbb717af183682afe29fdd5daa83101430e2573e
Eyyup Ucmaz 0291000445 <eyyup.ucmaz@netaszm.com.tr>
2022-08-22 12:06:29 +0300

    api call & image listing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
+import React from 'react';
+import { unsplash } from './api/unsplash';
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+import Loading from './components/Loading';
+import GradientText from './components/GradientText';
 
+interface AppStates {
+  imageData: Array<Object> | null;
+  status: number | null;
+  term: string;
+  loading: boolean | null;
+}
+class App extends React.Component<any, AppStates> {
+  constructor(props: Readonly<any>) {
+    super(props);
+    this.state = {
+      imageData: null,
+      status: null,
+      term: '',
+      loading: null,
+    };
+  }
 
-function App() {
-  return (
-    <div className="">
+  onSearchSubmit = async (term: string) => {
+    this.setState({ loading: true });
+    const { data, status } = await unsplash.get('search/photos', {
+      params: { query: term, per_page: 30 },
+    });
+    this.setState({
+      imageData: data?.results,
+      status: status,
+      term,
+      loading: false,
+    });
+  };
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="w-screen h-100 flex flex-col justify-center items-center pt-9">
+        <SearchBar onSubmit={this.onSearchSubmit} />
+
+        {this.state.loading ? (
+          <Loading />
+        ) : (
+          <>
+            {this.state.loading !== null && (
+              <GradientText
+                text={`${this.state.imageData?.length} images found for ${this.state.term}`}
+              />
+            )}
+            <ImageList images={this.state.imageData} />
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
